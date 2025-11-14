7289c122ca1db7719ffee55e501a6d1925c8a42d
Rakesh-dev07 <ry57520s@gmail.com>
2025-11-14 09:59:58 +0530

    rename file

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import About from "./components/About";
 import Hero from "./components/Hero";
-import Skills from "./components/Skills_team";
+import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
